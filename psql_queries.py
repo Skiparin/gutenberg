fa6e0b3faeb85313f9fb7b9794d88b9e827529f2
Guttesen <og1806x9@hotmail.com>
2018-05-22 13:56:41 +0200

    fixed

--- psql_queries.py
+++ psql_queries.py
@@ -86,7 +86,7 @@ def get_title_for_cords(x,y,r):
     result = conn.execute(text("""
         select b.title, c.name 
         from books b, cities c, books_cities bc
-        where circle'(("""+ x + ",": + y + ")," + r + """)' @> point(x_cord,y_cord) and
+        where circle'(("""+ x + "," + y + ")," + r + """)' @> point(x_cord,y_cord) and
         c.id = bc.city_id and
         bc.book_id = b.id;"""), x=x,y=y,r=r)
     array = []
