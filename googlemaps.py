8363c334d3651f7c6b988de872fe6d81bc3e71ee
Michael <2750daugbjerg@gmail.com>
2018-05-24 18:33:01 +0200

    working on sel testing

--- googlemaps.py
+++ googlemaps.py
@@ -72,6 +72,9 @@ def radius():
     if not x or not y or not r:
         flash('Please enter x and y coordinates with a radius')
         return redirect(url_for('index'))
+    if r > 5
+        flash('Please enter enter a smaller radius')
+        return redirect(url_for('index'))
     try:
         result = get_title_for_cords(x, y, r)
     except:
