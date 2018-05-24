3f3d27b99c9083b2e070c80d306342b5c914f803
skiparin <skiparin@github.com>
2018-05-24 21:48:40 +0000

    fix

--- test/test_database_proformance.py
+++ test/test_database_proformance.py
@@ -5,6 +5,8 @@ if parentPath not in sys.path:
     sys.path.insert(0, parentPath)
 from psql_queries import get_cities_for_title, get_titles_for_city, get_titles_and_cords_for_author, get_title_for_cords
 
+get_cities_for_title("Denmark")
+
 print("get_cities_for_title")
 for i in range(10):
 	start = time.time()
