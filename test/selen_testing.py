8952cc99aa417754cadf24fd91b1398f22f81fa4
Michael <2750daugbjerg@gmail.com>
2018-05-24 14:40:19 +0200

    fix

--- test/selen_testing.py
+++ test/selen_testing.py
@@ -7,11 +7,11 @@ import unittest
 class selen_testing(unittest.TestCase):
 
 	@classmethod
-	def setUpClass(cls, self):
-		cls.options = Options()
-		cls.options.add_argument("--headless")
+	def setUpClass(cls):
+		#cls.options = Options()
+		#cls.options.add_argument("--headless")
 
-		cls.driver = webdriver.Firefox(options=self.options, executable_path="geckodriver")
+		cls.driver = webdriver.Firefox(options=options.add_argument("--headless"), executable_path="geckodriver")
 		cls.driver.get("http://46.101.61.244:5000/")
 
 	def tearDown(self):
