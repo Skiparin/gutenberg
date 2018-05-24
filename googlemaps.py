f6addbab94cae1a47f7e4e06fcbf444ce9c23a32
Michael <2750daugbjerg@gmail.com>
2018-05-24 13:16:13 +0200

    error handling in googlemaps.py

--- googlemaps.py
+++ googlemaps.py
@@ -66,11 +66,14 @@ def radius():
     x = request.form['x']
     y = request.form['y']
     r = request.form['r']
+    result = get_title_for_cords(x, y, r)
     if not x or not y or not r:
         flash('Please enter x and y coordinates with a radius')
         return redirect(url_for('my_form'))
+    elif not result:
+        flash("No titles were found in this area")
+        return redirect(url_for('my_form'))
     else:
-        result = get_title_for_cords(x, y, r)
         return render_template('html/radius.html', result=result)
 
 @app.route("/map")
