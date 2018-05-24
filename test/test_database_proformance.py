b4f6fa2bc4be82a28f7875acc2a30889c8ebf301
Guttesen <og1806x9@hotmail.com>
2018-05-24 16:13:30 +0200

    added file

--- test/test_database_proformance.py
+++ test/test_database_proformance.py
@@ -1,8 +1,9 @@
 import os, sys
+import timeit
 parentPath = os.path.abspath("..")
 if parentPath not in sys.path:
     sys.path.insert(0, parentPath)
 from psql_queries import get_cities_for_title, get_titles_for_city, get_titles_and_cords_for_author, get_title_for_cords
 
-time = get_cities_for_title("Denmark").time()
+timeit.timeit('get_cities_for_title("Denmark")', number=10)
 print(time)
