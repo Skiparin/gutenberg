4f01baeb5fb1bb0ec9fba7273ce9cc6d780eba01
Michael <2750daugbjerg@gmail.com>
2018-05-25 12:31:07 +0200

    working on mongodb

--- db_migration.py
+++ db_migration.py
@@ -27,6 +27,6 @@ def getBooks():
     engine = db_connect()
     conn = engine.connect()
     result = conn.execute(text("""
-        COPY(SELECT id AS id.auto(), book AS book.auto(), title AS title.auto() FROM books) TO '/root/temp.csv WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');"""))
+        COPY(SELECT id AS 'id.auto()', book AS 'book.auto()', title AS 'title.auto()' FROM books) TO '/root/temp.csv WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');"""))
 
 getBooks()
