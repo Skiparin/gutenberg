196400617f2f3d16606d9968c45cb54ed59bc610
Michael <2750daugbjerg@gmail.com>
2018-05-25 15:52:03 +0200

    working on mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -16,7 +16,7 @@ cities = db.cities
 
 
 def get_titles_for_city(city):
-	cityFound = db.cities.find({'name': city, 'book_ids':'True'})
+	cityFound = db.cities.find({"name": city, 'book_ids':'True'})
 	result = db.books.find({'titles': {'$in': ['city_ids': 'True', cityFound]}})
 
 	print(result)
