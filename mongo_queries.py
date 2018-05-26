a7b5106833acf37b7ed8fa3aa5afcce217fca7e3
Michael <2750daugbjerg@gmail.com>
2018-05-26 12:55:36 +0200

    working on mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -32,6 +32,7 @@ def get_cities_for_title():
 	city_array = []
 	ids = books.find_one({"title": "Denmark"},{"city_ids":1})
 	city_result = cities.find({'_id': {'$in': ids['city_ids']}},{'x_cords': 1, 'y_cords': 1})
+	array = []
 	for r in city_result:
 		array.append((float(r[0]),float(r[1])))
 	print(array)
