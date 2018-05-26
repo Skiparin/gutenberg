bf3f8062a880c0f5d1092ca17cebe1545dc636b3
Michael <2750daugbjerg@gmail.com>
2018-05-26 12:21:55 +0200

    working on mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -18,18 +18,18 @@ cities = db.cities
 def get_titles_for_city():
 	book_array = []
 	ids = cities.find_one({"name": "Odense"},{'book_ids': 1})
-	book_results = books.find({'_id': {'$in': ids["book_ids"] }})
+	book_results = books.find({'_id': {'$in': ids["book_ids"] }}, {'title': 1, 'authors': 1})
 	for book in book_results:
 		print(book)
 		author_array = []
-		if 'author_ids' in book:
-			author_results = authors.find({'_id': {'$in': book['author_ids']}}, {'name': 1})
+		if 'authors' in book:
+			author_results = authors.find({'_id': {'$in': book['authors']}}, {'name': 1})
 			for author in author_results:
 				author_array.append(author)
 		temp_array = [book['title'], author_array]
 		book_array.append(temp_array)
-	print("**************************************'")
-	print(book_array)
+
+	#print(book_array)
 
 def get_titles_for_city1():
 	books_docs = []
