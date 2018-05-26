69fb0a27fd136e1b98f3c11348e2c130bc475457
Michael <2750daugbjerg@gmail.com>
2018-05-26 14:54:29 +0200

    working on mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -62,7 +62,7 @@ def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
     r_dict["cords"] = []
     for t in title_array:
         r_dict["titles"].append(t[0])
-        for float(x),float(y) in zip((t[1]),(t[2])):
+        for x,y in cord_array:
             r_dict["cords"].append((float(x),float(y)))
     return r_dict
 
