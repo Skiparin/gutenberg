c94cf08c743c2ec421039daab65b0f8238ddf771
Michael <2750daugbjerg@gmail.com>
2018-05-26 11:52:10 +0200

    working on mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -17,9 +17,8 @@ cities = db.cities
 
 def get_titles_for_city():
 	bookIds = db.cities.find_one({"name": "Odense"},{'book_ids': 1})
-	result = db.books.find({'title': {'$elemMatch': {'city_ids': 1, 'cities.book_ids': bookIds }}})
-	for title in result:
-		print("x")
+	result = db.books.find({'_id': {'$in': bookIds }})
+	print(result)
 
 def get_titles_for_city1():
 	books_docs = []
