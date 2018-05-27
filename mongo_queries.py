1cfd587793b327245d3a9d57437f39e066025d86
Michael <2750daugbjerg@gmail.com>
2018-05-27 18:53:27 +0200

    testing

--- mongo_queries.py
+++ mongo_queries.py
@@ -64,7 +64,6 @@ def get_title_for_cords(x,y,r):
 	book_result = books.find({'_id': {'$in': r_dict['book_ids']}}, {'title': 1})
 	for book in book_result:
 		book_array.append(book['title'])
-	print(book_array)
 	return(book_array)
 
 def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
@@ -79,4 +78,4 @@ def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
 
 #get_cities_for_title("Danger at the Drawbridge")
 #get_titles_and_cords_for_author("Max Simon Nordau")
-get_title_for_cords(55.795205, 12.472396, 0.15)
+#get_title_for_cords(55.795205, 12.472396, 0.15)
