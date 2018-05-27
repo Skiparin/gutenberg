31be7938b15570b376d7394c138a527b55e17918
Michael <2750daugbjerg@gmail.com>
2018-05-27 12:56:03 +0200

    fixing queries

--- mongo_queries.py
+++ mongo_queries.py
@@ -28,7 +28,7 @@ def get_titles_for_city(city):
 def get_cities_for_title(title):
 	city_array = []
 	ids = books.find_one({"title": title},{"city_ids":1})
-	city_result = cities.find({'_id': {'$in': ids['city_ids'] }},{'location': {'coordinates': 1}})
+	city_result = cities.find({'_id': {'$in': ids['city_ids'] }},{'coordinates':[1, 1]})
 	array = []
 	for c in city_result:
 		array.append([c[0], c[1]])
