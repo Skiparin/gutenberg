c2291e5ad3c3f98d6b56513f5ec829f90eb47b96
Michael <2750daugbjerg@gmail.com>
2018-05-27 16:47:36 +0200

    mongodb

--- mongo_queries.py
+++ mongo_queries.py
@@ -45,23 +45,16 @@ def get_titles_and_cords_for_author(author):
 	for cord in city_result:
 		cord_array.append((float(cord["location"]["coordinates"][0]), float(cord["location"]["coordinates"][1])))
 	r_dict = get_titles_and_cords_for_author_to_dict(title_array, cord_array)
-	print(r_dict)
 	return r_dict
 
 def get_title_for_cords():
 	book_array = []
-	location_array = []
-	ids = cities.find({'_id': {'$geoNear': {'$center': {'x_cord': 50, 'y_cord': 20}}}}, {'book_ids': 1})
-	for book in ids:
-		location_array.append(book['book_ids'])
-	print(location_array)
-	"""
-	ids = cities.find({'_id': {'type': 'point' , ['x_cord': 50, 'y_cord': 20]}}, {'book_ids': 1})
+	#ids = cities.find({'_id': {'$geoNear': {'$center': {'x_cord': 50, 'y_cord': 20}}}}, {'book_ids': 1})
+	ids = db.cities.find({'location': { '$near': {'$geometry': { 'type': "Point", 'coordinates': [ -73.9667, 40.78 ] },'$maxDistance': 5000}}}, {'book_ids': 1})
 	book_result = books.find({'_id': {'$in': ids['book_ids']}}, {'title': 1})
-	for title in book_results:
-		book_result.append(title['title'])
+	for book in book_result:
+		book_array.append(book['title'])
 	print(book_array)
-	"""
 
 def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
 	r_dict = {}
@@ -74,5 +67,5 @@ def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
 	return r_dict
 
 #get_cities_for_title("Danger at the Drawbridge")
-get_titles_and_cords_for_author("Max Simon Nordau")
-#get_title_for_cords()
+#get_titles_and_cords_for_author("Max Simon Nordau")
+get_title_for_cords()
