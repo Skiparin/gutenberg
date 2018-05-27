1214a653e4e508b186433f0bff1cbac6d64736a8
Michael <2750daugbjerg@gmail.com>
2018-05-27 18:24:26 +0200

    test

--- mongo_queries.py
+++ mongo_queries.py
@@ -78,4 +78,4 @@ def get_titles_and_cords_for_author_to_dict(title_array, cord_array):
 
 #get_cities_for_title("Danger at the Drawbridge")
 #get_titles_and_cords_for_author("Max Simon Nordau")
-get_title_for_cords(1, 1, 1000000)
+get_title_for_cords(1, 1, 100000)
