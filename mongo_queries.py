a9d3f1b75a4b5eea8d0ad3e3b3edd1cb8230725a
Michael <2750daugbjerg@gmail.com>
2018-05-27 12:58:15 +0200

    fixing queries

--- mongo_queries.py
+++ mongo_queries.py
@@ -31,7 +31,7 @@ def get_cities_for_title(title):
 	city_result = cities.find({'_id': {'$in': ids['city_ids'] }},{'coordinates':[1, 1]})
 	array = []
 	for c in city_result:
-		array.append(float(c[0]), float(c[1]))
+		array.append(c)
 
 		#array.append((float(r['coordinates'[0]]),float(r['coordinates'[1]])))
 	print(array)
